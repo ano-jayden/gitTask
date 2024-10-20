c585d0c2ef9a264eaeda078c6f018cef11f804f6
ajaxp <anoyakutira15022@gmail.con>
2024-10-21 01:28:21 +0200

    Added docstrings to views, models, and forms, and updated conf.py

--- plate/conf.py
+++ plate/conf.py
@@ -3,6 +3,18 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import os
+import sys
+
+# Add the root of your Django project to the Python path
+sys.path.insert(0, os.path.abspath('../'))  # Adjust the path if necessary
+
+# Refers to plate/settings.py
+os.environ['DJANGO_SETTINGS_MODULE'] = 'plate.settings'
+
+import django
+django.setup()  # This ensures that Django settings are correctly loaded
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
@@ -18,8 +30,6 @@ extensions = []
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
-
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
